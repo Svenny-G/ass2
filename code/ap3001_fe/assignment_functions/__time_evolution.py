187e5631269470b1c8553d49aa926461be8a9795
Svenny-G <sven.goudriaan@gmail.com>
2025-01-17 13:07:09 +0100

    Assignment 8 implemented

--- code/ap3001_fe/assignment_functions/__time_evolution.py
+++ code/ap3001_fe/assignment_functions/__time_evolution.py
@@ -77,8 +77,8 @@ def evolve_in_time(u_0, f, M, N, delta_t, theta, basis, tol=1e-10, t_step_max = 
     #
     # Note 2: ignore the essential boundary conditions for now, these will be set below,
     #         i.e., consider the system without the essential boundary conditions at this stage.
-    A = # TODO
-    B = # TODO
+    A = 1/delta t*(M+delta_t*theta*N) # TODO
+    B = 1/delta_t*(M-delta_t*(1-theta)*N) # TODO
 
     # Impose boundary conditions in A
     A_bc = assembly.impose_boundary_conditions_matrix(A, basis)
